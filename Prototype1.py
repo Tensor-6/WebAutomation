6cc5855a18e36d467da3c8e33d5f01f6c66d17d5
Tensor-6 <124675744+Tensor-6@users.noreply.github.com>
2024-03-06 09:03:17 -0800

    Webbrowser

---
+++ Prototype1.py
@@ -0,0 +1,8 @@
+url = [
+       "https://cityofsurrey.perfectmind.com/23615/Clients/BookMe4LandingPages/Class?widgetId=b4059e75-9755-401f-a7b5-d7c75361420d&classId=86f807a8-592f-4c42-b8f3-8a0fa95c6e32&occurrenceDate=20240308",
+
+       ]
+
+import webbrowser
+
+webbrowser.open(url[0])  # Go to example.c
