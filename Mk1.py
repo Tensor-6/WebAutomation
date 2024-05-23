1fa9e9e12469bc8e16d9b38869b31c07b8fd9e5c
Tensor-6 <124675744+Tensor-6@users.noreply.github.com>
2024-05-23 11:35:34 -0700

    h

--- Mk1.py
+++ Mk1.py
@@ -9,8 +9,13 @@ print(1)
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+url = [
+       "?widgetId=b4059e75-9755-401f-a7b5-d7c75361420d&redirectedFromEmbededMode=False&classId=c1a62643-ec0e-a373-347f-14d0a91315fb&occurrenceDate=20240524",
+
+       ]
+
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
+driver.get(url[0] )
 assert "Python" in driver.title
 elem = driver.find_element(By.NAME, "q")
 elem.clear()
