a6277911aa5f95c1d025a400a136f8775baa82e3
Tensor-6 <Kelper22c@gmail.com>
2024-05-24 08:28:42 -0700

    mk1.5

--- Mk1.py
+++ Mk1.py
@@ -1,24 +1,40 @@
 import ModLoad
-
-depen = ['selenium']
+import time
+depen = ['selenium',
+       'cryptography'
+       ]
 ModLoad.importDependencies(depen)
 
+import base64
 import selenium
 from selenium import webdriver
-print(1)
-from selenium.webdriver.common.keys import Keys
+from cryptography.fernet import Fernet
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.fernet import Fernet
 
 url = [
-       "?widgetId=b4059e75-9755-401f-a7b5-d7c75361420d&redirectedFromEmbededMode=False&classId=c1a62643-ec0e-a373-347f-14d0a91315fb&occurrenceDate=20240524",
-
+       "https://cityofsurrey.perfectmind.com/23615/Clients/BookMe4LandingPages/Class?widgetId=b4059e75-9755-401f-a7b5-d7c75361420d&redirectedFromEmbededMode=False&classId=c1a62643-ec0e-a373-347f-14d0a91315fb&occurrenceDate=20240524",
+       "https://cityofsurrey.perfectmind.com/23615/Clients/BookMe4LandingPages/Class?widgetId=b4059e75-9755-401f-a7b5-d7c75361420d&redirectedFromEmbededMode=False&classId=32db5eaa-69d6-f210-e8ce-40a81856dff7&occurrenceDate=20240524"
        ]
 
-driver = webdriver.Firefox()
-driver.get(url[0] )
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver = webdriver.Chrome()
+driver.get(url[1] )
+time.sleep(10)
+#anchor = driver.find_element(By.CLASS_NAME, 'bm-button bm-book-button')
+#anchor.click()
+anchor_tag = driver.find_element(By.CSS_SELECTOR, '.bm-button.bm-book-button')
+anchor_tag.click()
+time.sleep(5)
+LogInEmlIn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'emailid')))
+LogInEmlIn.send_keys('testa@g.vb')
+LogInPslIn = driver.find_element(By.ID, 'loginradius-login-password')
+LogInPslIn.send_keys('testa@g.vb')
+SignInBtn =  driver.find_element(By.ID, 'loginradius-validate-login')
+time.sleep(6000)
+driver.quit()
---
+++ Untitled-1.php
@@ -0,0 +1,36 @@
+<section class="bm-booking-info bm-restrictions">
+             
+            
+
+    <a class="bm-button bm-book-button" href="/23615/Clients/BookMe4EventParticipants?eventId=32db5eaa-69d6-f210-e8ce-40a81856dff7&amp;occurrenceDate=20240524&amp;widgetId=b4059e75-9755-401f-a7b5-d7c75361420d&amp;locationId=a89fe9f3-5ece-4158-a87d-c61ec1e99601&amp;waitListMode=False" aria-label="Register Drop In Basketball - Adult" tabindex="0">
+    Register
+    </a>
+    <div id="registrationRestrictionAlertPopup">
+        <span class="close-button"><em class="fa fa-times" aria-hidden="true"></em></span>
+    </div>
+
+<div class="common-restrictions" tabindex="0">
+                    <div class="registration-info-header">Restrictions</div>
+                  
+
+                 
+                    <div class="registration-info-text">Age: 19 to 105</div>
+                 
+
+                 
+                              
+            </div>
+            
+
+        
+
+            <br>
+            <br>
+            <br>
+
+            <div class="registration-common-info" aria-label="Registration ends on May 24th, 2024 at 06:45 PM">
+                <div>Registration ends on 24-May-2024 at 06:45 PM</div>
+            </div>
+         
+        <section>
+    </section></section>
